822c1b75fbff48bf1ec6ec88ff7d62b49cd79be7
Rohit-2301 <22bcs13677@cuchd.in>
2025-03-19 12:01:00 +0530

    Create WordBreak.cpp

---
+++ WordBreak.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool wordBreak(string s, vector<string>& wordDict) {
+    unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+    vector<bool> dp(s.size() + 1, false);
+    dp[0] = true;
+
+    for (int i = 1; i <= s.size(); i++) {
+        for (int j = 0; j < i; j++) {
+            if (dp[j] && wordSet.count(s.substr(j, i - j))) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+    return dp[s.size()];
+}
